34a8a7111156f134edc5894325649de4a1db65b1
itcyh <554913287@qq.com>
2015-02-02 22:21:25 +0800

    Create process.php

---
+++ process.php
@@ -0,0 +1,12 @@
+<?php
+$text = "The data that you submit:";
+$text .= "<br>Username: " . $_POST['username'];
+$text .= "<br>Password: " . $_POST['password'];
+$text .= "<br>Gender: " . $_POST['gender'];
+$text .= "<br>Textarea: " . $_POST['message'];
+$text .= "<br>Interest:";
+for($i=0; $i<count($_POST["interest"]); $i++) {
+    $text .= " " . $_POST["interest"][$i];
+}
+echo $text;
+?>
